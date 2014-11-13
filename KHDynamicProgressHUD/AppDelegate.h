9afbd411ae5e7ae53998889ec8e04e3123470709
Yundi Huang <kenkaka.hyd@gmail.com>
2014-11-13 18:08:56 -0500

    Initial Commit

---
+++ KHDynamicProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KHDynamicProgressHUD
+//
+//  Created by Ken Huang on 2014-11-13.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KHDynamicProgressHUD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KHDynamicProgressHUD
+//
+//  Created by Ken Huang on 2014-11-13.
+//  Copyright (c) 2014 Ken Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
